628f226b164633e9b3dfca079d8ebf194f3f7a99
JonatanV <nofettatjarn1>
2019-04-10 11:54:18 +0200

    kan gå men inte rätt

--- src/snake.java
+++ src/snake.java
@@ -4,17 +4,18 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
 
-public class snake extends Canvas {
-    public static void main(String[] args) {
-        snake minGrafik=new snake();
-    }
-    int height=600;
-    int width=800;
-    int x=100;
-    int y=100;
+public class snake extends Canvas implements Runnable {
+    int height = 600;
+    int width = 800;
+    int x = 100;
+    int y = 100;
     BufferStrategy bs;
-    public snake(){
-        setSize(width,height);
+    private Thread thread;
+    private boolean running = false;
+
+
+    public snake() {
+        setSize(width, height);
         JFrame frame = new JFrame("Grafik");
         frame.add(this);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -22,44 +23,95 @@ public class snake extends Canvas {
         frame.setVisible(true);
         frame.addKeyListener(new kl());
     }
-    public void paint (Graphics g){
+
+    public synchronized void start() {
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void render() {
         bs = getBufferStrategy();
-        if(bs == null) {
+        if (bs == null) {
             createBufferStrategy(1);
             return;
         }
-        update(g);
+        Graphics g = bs.getDrawGraphics();
         draw(g);
         g.dispose();
         bs.show();
 
-    BufferStrategy bs;   repaint();
-    }
-    public void draw (Graphics g){
-        g.setColor(new Color(0x000));
-        g.fillRect(0,0,800,600);
-        snakePlayer(g);
-    }
-    private void snakePlayer(Graphics g){
-        g.setColor(Color.RED);
-        g.fillRect(x,y,15,15);
-    }
-    public void update(Graphics g){
-        x++;
     }
 
-    private class kl implements KeyListener {
+    public void draw(Graphics g) {
+        g.setColor(new Color(0xfffffff));
+        g.fillRect(0,0,width,height);
+        g.setColor(Color.RED);
+        g.fillRect(x, y, 15, 15);
+    }
+
+    public void update() {
+
+    }
+
+    @Override
+    public void run() {
+        double ns = 1000000000.0 / 30.0;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+
+            while (delta >= 1) {
+                // Uppdatera koordinaterna
+                update();
+                // Rita ut bilden med updaterad data
+                render();
+                delta--;
+            }
+        }
+        stop();
+    }
+
+
+    public static void main(String[] args) {
+        snake minGrafik = new snake();
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                minGrafik.setVisible(true);
+            }
+        });
+        minGrafik.start();
+    }
+
+
+    class kl implements KeyListener {
         @Override
         public void keyTyped(KeyEvent keyEvent) {
-            if (keyEvent.getKeyChar()=='a'){
+            if (keyEvent.getKeyChar() == 'a') {
+                x--;
+            } else if (keyEvent.getKeyChar() == 'w') {
+                y--;
 
-            }else if(keyEvent.getKeyChar()=='w'){
-
-            }else if(keyEvent.getKeyChar()=='s'){
+            } else if (keyEvent.getKeyChar() == 's') {
                 y++;
-            }else if(keyEvent.getKeyChar()=='d'){
 
-            }else if (keyEvent.getKeyChar()=='\n'){
+            } else if (keyEvent.getKeyChar() == 'd') {
+                x++;
+
+            } else if (keyEvent.getKeyChar() == '\n') {
 
             }
         }
---
+++ src/snek.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class snek extends Canvas {
+    int heigt=100;
+    int width=100;
+    int x,y;
+    int x1=100;
+    int y1=100;
+
+    public static void main(String[] args) {
+        snake minGrafik=new snake();
+    }
+}
